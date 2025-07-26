2387b7937868b93a2e95fb2e246c0ccd12582583
kalyan2312 <reply2kalyan@outlook.com>
2025-07-26 22:17:05 +0530

    updated gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my_portfolio/', // <-- This is required for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
